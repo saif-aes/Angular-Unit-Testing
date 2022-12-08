c50b3b3f3aab675c1810416ecbc673766b720a05
aes-saif <saif.chowdhury@anwargroup.net>
2022-12-08 12:12:45 +0600

    hello test

---
+++ src/app/first-test.spec.ts
@@ -0,0 +1,25 @@
+describe('First Test', () => {
+  let testVariable: any;
+
+  beforeEach(() => {
+    testVariable = {};
+  });
+
+  it("should return true if 'a' is true", () => {
+    // arrange
+    testVariable.a = false;
+
+    // act
+    testVariable.a = true;
+
+    // assert
+    expect(testVariable.a).toBe(false);
+  });
+});
+
+// User service -> getUser() method -> should return the correct given user
+describe('User Service', () => {
+  describe('getUser() method', () => {
+    it('should return the correct given user', () => {});
+  });
+});
