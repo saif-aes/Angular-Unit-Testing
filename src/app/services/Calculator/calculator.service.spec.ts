e120ba320388694d3fbf51d8c2092f6a4ce6d843
aes-saif <saif.chowdhury@anwargroup.net>
2022-12-11 13:15:05 +0600

    using beforeEach(callback)

--- src/app/services/Calculator/calculator.service.spec.ts
+++ src/app/services/Calculator/calculator.service.spec.ts
@@ -1,23 +1,28 @@
-import { LoggerService } from '../Logger/logger.service';
 import { CalculatorService } from './calculator.service';
 
 describe('Calculator Service', () => {
-  it('It should add two numbers', () => {
-    let mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
+  let mockLoggerService: any;
+  let calc: any;
 
-    const calc = new CalculatorService(mockLoggerService);
+  beforeEach(() => {
+    console.log('calling beforeEach');
+    mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
+    calc = new CalculatorService(mockLoggerService);
+  });
+
+  it('It should add two numbers', () => {
+    console.log('calling add');
+
     const result = calc.add(2, 3);
     expect(result).toBe(5);
     expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
   });
 
   it('It should subtract two numbers', () => {
-    let loggerService = new LoggerService();
+    console.log('calling subtract');
 
-    spyOn(loggerService, 'log');
-    const calc = new CalculatorService(loggerService);
     const result = calc.subtract(2, 3);
     expect(result).toBe(-1);
-    expect(loggerService.log).toHaveBeenCalledTimes(1);
+    expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
   });
 });
