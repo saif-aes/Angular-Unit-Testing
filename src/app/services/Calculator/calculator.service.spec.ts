536fd9b5fe93b8bbb266b4a298d2dcb3d5917772
aes-saif <saif.chowdhury@anwargroup.net>
2022-12-11 13:00:31 +0600

    Mock service implemented

--- src/app/services/Calculator/calculator.service.spec.ts
+++ src/app/services/Calculator/calculator.service.spec.ts
@@ -1,15 +1,23 @@
+import { LoggerService } from '../Logger/logger.service';
 import { CalculatorService } from './calculator.service';
 
 describe('Calculator Service', () => {
   it('It should add two numbers', () => {
-    const calc = new CalculatorService();
+    let mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
+
+    const calc = new CalculatorService(mockLoggerService);
     const result = calc.add(2, 3);
     expect(result).toBe(5);
+    expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
   });
 
   it('It should subtract two numbers', () => {
-    const calc = new CalculatorService();
+    let loggerService = new LoggerService();
+
+    spyOn(loggerService, 'log');
+    const calc = new CalculatorService(loggerService);
     const result = calc.subtract(2, 3);
     expect(result).toBe(-1);
+    expect(loggerService.log).toHaveBeenCalledTimes(1);
   });
 });
--- src/app/services/Calculator/calculator.service.ts
+++ src/app/services/Calculator/calculator.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
+import { LoggerService } from '../Logger/logger.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CalculatorService {
+  constructor(private loggerService: LoggerService) {}
+
   add(n1: number, n2: number) {
+    this.loggerService.log('Add operation is called');
     return n1 + n2;
   }
 
   subtract(n1: number, n2: number) {
+    this.loggerService.log('Subtract operation is called');
     return n1 - n2;
   }
 }
